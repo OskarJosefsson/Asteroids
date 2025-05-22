d6ab23713e691845edeb00c3ae0c2cddec5b5b82
OskarJosefsson <oskar.josefsson12@gmail.com>
2025-05-22 22:02:55 +0200

    filestruct

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ from constants import SCREEN_WIDTH , SCREEN_HEIGHT
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    clock.dt = 0
+
     print("Starting Asteroids!")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
@@ -17,6 +20,7 @@ def main():
                              return
                 
         screen.fill((0, 0, 0))
+        clock.dt = clock.tick(60) / 1000
         pygame.display.flip()
 
 
